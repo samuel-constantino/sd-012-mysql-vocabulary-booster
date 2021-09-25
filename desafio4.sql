3740eec87361f97c78fb316c653c421612b844f5
Samuel Constantino <samuelicapuidados@gmail.com>
2021-09-25 17:07:10 -0300

    refatora desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,24 +1,12 @@
 SELECT 
-    j.job_title AS Cargo,
+    j.JOB_TITLE AS 'Cargo',
     ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
-        WHEN
-            ROUND(AVG(e.SALARY), 2) >= 2000
-                AND ROUND(AVG(e.SALARY), 2) <= 5800
-        THEN
-            'Júnior'
-        WHEN
-            ROUND(AVG(e.SALARY), 2) >= 5801
-                AND ROUND(AVG(e.SALARY), 2) <= 7500
-        THEN
-            'Pleno'
-        WHEN
-            ROUND(AVG(e.SALARY), 2) >= 7501
-                AND ROUND(AVG(e.SALARY), 2) <= 10500
-        THEN
-            'Sênior'
-        WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
-    END AS Senioridade
+        WHEN ROUND(AVG(e.SALARY), 2) < 5800.00 THEN 'Júnior'
+        WHEN ROUND(AVG(e.SALARY), 2) < 7500.00 THEN 'Pleno'
+        WHEN ROUND(AVG(e.SALARY), 2) < 10500.00 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
 FROM
     hr.jobs AS j
         INNER JOIN
