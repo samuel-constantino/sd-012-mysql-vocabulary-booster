1e96976401b4656e25a50e91fd2ca2e1404e4571
Samuel Constantino <samuelicapuidados@gmail.com>
2021-09-25 17:24:33 -0300

    implementa desafio 5: classificar por variação e medias salariais

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT 
+    j.JOB_TITLE AS Cargo,
+    (j.MAX_SALARY - j.MIN_SALARY) AS 'Variação Salarial',
+    ROUND(AVG(j.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+    ROUND(AVG(j.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+FROM
+    hr.jobs AS j
+GROUP BY j.JOB_ID
+ORDER BY (j.MAX_SALARY - j.MIN_SALARY), Cargo;
